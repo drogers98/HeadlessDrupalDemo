3521ed26714a4db3ff796da25fe386cba5d32081
johnvehr <johnvehr@yahoo.com>
2014-09-09 11:40:11 -0600

    small fixes to work

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -6,21 +6,21 @@ angular.module('YouthworksMobil.services', [])
 .factory('EventService', function($http) {
   // Might use a resource here that returns a JSON array
  
-    
+
     var events = [];
-  
+
 
     $http.get('http://test-youth-works.gotpantheon.com/api/views/announcements.json').success(function(data) {
         // you can do some processing here
         var events = data;
        console.log(events);
-       
+
     });
 
   return {
     all: function() {
       return events;
-       
+
     },
     get: function(eventId) {
       // Simple index lookup
